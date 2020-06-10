95f9e150e0185d397242632d5103593b6ba07dab
indahmanik <60678765+indahmanik@users.noreply.github.com>
2020-06-10 22:23:55 +0700

    Update Buah.java

--- Lemon Shop/src/lemon/shop/Buah.java
+++ Lemon Shop/src/lemon/shop/Buah.java
@@ -1,33 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package lemon.shop;
 
 import java.util.Date;
 
 /**
  *
- * @author Kelompok08
+ * @author kelompok08
  */
 
 public class Buah {
-    private String kodeBuah;
-    private int harga;
-    private Date arrivalDate;
-    private String expiredDate;
-    private int jumlahStok;
+   private int kodeBuah = 0;
+   private String namaBuah;
+   private double harga;
+   private Date arrivalDate;
+   private String expiredDate;
+   private int jumlah;
     
-    public void Buah(String kodeBuah, int harga, Date arrivalDate, String expiredDate, int jumlahStok){
-        this.kodeBuah = kodeBuah;
-        this.arrivalDate = arrivalDate;
-        this.expiredDate = expiredDate;
-        this.jumlahStok = jumlahStok;
+    public Buah(){
+        this(0,"Unknown",0,0.00);
     }
     
-    public void getDiscount(){
-        this.expiredDate = null;
-        this.kodeBuah = null;
+    public Buah(int kodeBuah, String namaBuah, int harga, Date arrivalDate, String expiredDate, int jumlahStok){
+        kodeBuah = kodeBuah;
+        setNamaBuah(namaBuah);
+        setJumlahStok(jumlahStok);
+        setArrivalDate(arrivalDate);
+        setExpiredDate(expiredDate);
+        setHarga(harga);
     }
     
-    public void getHarga(int harga){
-        this.harga = harga;
-        this.kodeBuah = null;
-    }
+public void setNamaBuah(String namaBuah){
+    namaBuah = namaBuah;
 }
+
+public void setJumlahStok(int jumlahStok){
+    if(jumlahStok > 0){
+        jumlah = jumlahStok;
+    }
+    else{jumlah = 0;}
+}
+
+public void setHarga(double harga){
+    if(harga > 0.00){
+        harga = harga;
+    }
+    else{harga = 0.00;}
+}
+
+public setArrivalDate(Date arrivalDate){
+    today.getDate();
+}
+    
+public setExpiredDate(String expiredDate){
+    expiredDate = expiredDate;
+}
+
+public String getNamaBuah(){
+    return namaBuah;
+}
+
+public int getJumlahStok(){
+    return jumlah;
+}
+
+public double getHarga(){
+    return harga;
+}
+
+public Date arrivalDate(){
+    return arrivalDate;
+}
+
+public String expiredDate(){
+    return expiredDate;
+}
+
+public double getItemValue(){
+    return (harga * (double)jumlah);
+}
+
+public String toString(){
+    return namaBuah + " - " + harga;
+}
+}
+
