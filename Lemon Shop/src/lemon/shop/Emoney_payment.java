c5a5a5331689c76b7cdab118f2fd46f6509a6fcd
indahmanik <60678765+indahmanik@users.noreply.github.com>
2020-06-11 21:50:03 +0700

    Update Emoney_payment.java

--- Lemon Shop/src/lemon/shop/Emoney_payment.java
+++ Lemon Shop/src/lemon/shop/Emoney_payment.java
@@ -10,15 +10,38 @@ package lemon.shop;
  * @author ACER
  */
 public class Emoney_payment {
-  String kodePembayaran;
-  String JenisEmoney;
-  
-  public Emoney_payment(String kodePembayaran, String JenisEmoney){
-      this.kodePembayaran = kodePembayaran;
-      this.JenisEmoney = JenisEmoney;
-  }
-  
-  public void getKodeTransaksi(){
-      this.kodePembayaran = null;
-  }
+    private String name; 
+    private int accountNumber; 
+    private double transactionCode; 
+    private String paymentType; 
+    private double beginningBalance; 
+    private double endBalance; 
+    private double creditLimit; 
+    private int numberPurchases; 
+    private int numberTransactions; 
+    private double costOfPurchase; 
+    private double totalPurchaseSum; 
+    private double totalPaymentAmount;
+    private double totalPayments; 
+
+        public double getTransaction()
+{
+    if (transactionCode.equals("1")) //if a purchase is being made
+    {
+        if (paymentType.equals("cash")) 
+            return endBalance = ;
+        else if (paymentType.equals("credit")) 
+            return creditLimit = getCreditLimit(creditLimit);
+    }
+    if (transactionCode = 2) 
+    {
+        if (paymentType.equals("cash") //
+            return endBalance = getEndBalance();        
+        else if (paymentType.equals("credit")) 
+    }
+    if (transactionCode = 0) 
+    {   
+        System.out.println("TRANSACTION COMPLETED. THANK YOU.")
+        return;
+    }
 }
