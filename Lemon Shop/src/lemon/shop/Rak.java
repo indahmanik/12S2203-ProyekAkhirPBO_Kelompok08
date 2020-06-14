9d0b6c91c7ed1290ec6933606a5be4f1e1f92220
indahmanik <60678765+indahmanik@users.noreply.github.com>
2020-06-14 14:50:32 +0700

    Update Rak.java

--- Lemon Shop/src/lemon/shop/Rak.java
+++ Lemon Shop/src/lemon/shop/Rak.java
@@ -9,17 +9,67 @@ package lemon.shop;
  *
  * @author ACER
  */
+
+import java.util.ArrayList;
+
 public class Rak {
-    private String kodeRak;
+    private ArrayList<String> Buah = new ArrayList<>();
+    private String kodeBuah;
     private String lokasi;
     
-    public void Rak(String kodeRak, String lokasi){
-        this.kodeRak = kodeRak;
-        this.lokasi = lokasi;
-    }
+ public Rak(int kodeBuah) {
+		if (kodeBuah > 0) {
+			this.kodeBuah = kodeBuah;
+		} else {
+			throw new IllegalArgumentException("kode yang dimasukkan tidak valid");
+		}
+	}
     
-    public void getLocation(){
-        this.kodeRak = null;
-        this.lokasi = null;
-    }
+    private synchronized boolean isFull() {
+		return Buah.size() == this.kodeBuah;
+	}
+
+	private synchronized boolean isEmpty() {
+		return Buah.isEmpty();
+	}
+
+	public synchronized void petugas_restock (String Buah) {
+		if (isFull()) {
+			System.out
+					.println(" Buah tidak dapat ditambahkan!!");
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println("Interruption");
+			}
+		}
+		Buah.add(Buah);
+		System.out.printf("Buah : %s ditambahkan !!!%n", Buah);
+	}
+
+	public synchronized String pembeli() {
+
+		if (isEmpty()) {
+			System.out.println("Buah habis");
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				System.out.println("interruption occure !!!");
+			}
+		}
+		
+		String currentFruitRecusted = Buah.remove(0);
+		notifyAll();
+		return currentFruitRecusted;
+	}
+
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Rak superRak = new Rak(10);
+		
+		
+		
+	}
 }
