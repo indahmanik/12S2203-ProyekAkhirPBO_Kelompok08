193a33948136f848195f800d28634dfbb6f72c3c
Andika-MFT <60682329+Andika-MFT@users.noreply.github.com>
2020-06-15 21:53:18 +0700

    Add files via upload

---
+++ Lemon Shop/src/Koneksi/Koneksi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Koneksi;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Kelompok_08
+ */
+public class Koneksi {
+  private static Connection KoneksiDatabse;
+    public static Connection koneksiDB() throws SQLException {
+        try {
+            String DB ="jdbc:mysql://localhost/pbo lemon?user=root&password=andika";
+            String user="root";
+            String pass="andika";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            KoneksiDatabse = (Connection) DriverManager.getConnection(DB);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,"Tidak ada koneksi","Error",
+            JOptionPane.INFORMATION_MESSAGE);
+            System.err.println(e.getMessage());
+            System.exit(0);
+        }
+         return KoneksiDatabse;
+    }  
+}
